30064c4735bd85197e3f1b9c03e629ff49c3bb0e
Måns Alskog <manal270@student.liu.se>
2022-11-23 23:27:39 +0100

    Standards compliant

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
+S=20
 all:
 	gcc -Wall -Wextra -pedantic -std=c89 main.c -o main
 	./main avatar.ppm
+	wc -c main.c | awk '{for (M = 0; 3*M*M < $$1; M++); print M, 3*M*M - $$1}'
 	diff main.c avatar.ppm || true
-	ffmpeg -loglevel warning -hide_banner -i avatar.ppm -update 1 -y avatar.png
+	ffmpeg -loglevel warning -hide_banner \
+		-i avatar.ppm\
+		-update 1 -y\
+		-vf "scale=$(S)*iw:$(S)*ih" -sws_flags neighbor\
+		avatar.png
 clean:
 	rm -f main avatar.ppm avatar.png
--- main.c
+++ main.c
@@ -9,40 +9,48 @@ int main(int c, char **a) {
         "    };",
         "    char m[] = {'w', 'b', 0};",
         "    char s[] = {'P', '6', ' ', '%', 'd', ' ', '%', 'd', ' ', '%', 'd', 0};",
-        "    char t[] = {'%', '8', 's', 34, '%', 's', 34, ',', 0};",
-        "    int i, j, M = 23, N = sizeof l / sizeof *l;",
+        "    char *t;",
+        "    int i, j, k, M = 23, N = sizeof l / sizeof *l;",
         "    if (c < 2) return 0;",
         "    freopen(a[1], m, stdout);",
         "    printf(s, M, M, 255);",
         "    puts(m + 2);",
         "    for (i = 0; i < N; i++) {",
         "        if (i == 4) for (j = 0; j < N; j++) {",
-        "            if (j == N - 1) t[7] = 0;",
-        "            printf(t, m + 2, l[j]);",
-        "            puts(m + 2);",
+        "            for (k = 0; k < 8; k++) putchar(' ');",
+        "            putchar('\\\"');",
+        "            for (t = l[j]; *t; t++) {",
+        "                if (*t == '\\\"' || *t == '\\\\') putchar('\\\\');",
+        "                putchar(*t);",
+        "            }",
+        "            puts(j < N - 1 ? \"\\\",\" : \"\\\"\");",
         "        }",
         "        puts(l[i]);",
         "    }",
         "    fclose(stdout);",
         "    return 0;",
-        "}              "
+        "}      "
     };
     char m[] = {'w', 'b', 0};
     char s[] = {'P', '6', ' ', '%', 'd', ' ', '%', 'd', ' ', '%', 'd', 0};
-    char t[] = {'%', '8', 's', 34, '%', 's', 34, ',', 0};
-    int i, j, M = 23, N = sizeof l / sizeof *l;
+    char *t;
+    int i, j, k, M = 23, N = sizeof l / sizeof *l;
     if (c < 2) return 0;
     freopen(a[1], m, stdout);
     printf(s, M, M, 255);
     puts(m + 2);
     for (i = 0; i < N; i++) {
         if (i == 4) for (j = 0; j < N; j++) {
-            if (j == N - 1) t[7] = 0;
-            printf(t, m + 2, l[j]);
-            puts(m + 2);
+            for (k = 0; k < 8; k++) putchar(' ');
+            putchar('\"');
+            for (t = l[j]; *t; t++) {
+                if (*t == '\"' || *t == '\\') putchar('\\');
+                putchar(*t);
+            }
+            puts(j < N - 1 ? "\"," : "\"");
         }
         puts(l[i]);
     }
     fclose(stdout);
     return 0;
-}              
+}      
